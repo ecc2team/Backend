3f51954636673681a47d3fd8876fa54da536bb4e
mirikim404 <mirikim8989@gmail.com>
2026-08-05 13:40:58 +0900

    feat: User 레포지토리 추가

---
+++ src/main/java/com/zeropick/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.zeropick.backend.repository;
+
+import com.zeropick.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailAndDeletedAtIsNull (String email);
+    boolean existsByEmailAndDeletedAtIsNull (String email);
+}
