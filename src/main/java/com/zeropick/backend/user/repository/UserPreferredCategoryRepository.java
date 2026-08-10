3c16dcf1866f93b3a9b70c8bd78870aba0e29457
mirikim404 <mirikim8989@gmail.com>
2026-08-11 02:19:27 +0900

    fix: 제네릭 타입 수정

--- src/main/java/com/zeropick/backend/user/repository/UserPreferredCategoryRepository.java
+++ src/main/java/com/zeropick/backend/user/repository/UserPreferredCategoryRepository.java
@@ -1,7 +1,8 @@
 package com.zeropick.backend.user.repository;
 
+import com.zeropick.backend.user.entity.UserPreferredCategory;
 import com.zeropick.backend.user.entity.UserPreferredIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserPreferredCategoryRepository extends JpaRepository<UserPreferredIngredient, Long> {
+public interface UserPreferredCategoryRepository extends JpaRepository<UserPreferredCategory, Long> {
 }
