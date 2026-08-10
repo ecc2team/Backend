fbc7bd25f26a1bc26001eaef190ff6dd8f54acfa
mirikim404 <mirikim8989@gmail.com>
2026-08-11 02:16:19 +0900

    feat: category 엔티티, 레포지토리 구현

--- src/main/java/com/zeropick/backend/category/Category.java
+++ src/main/java/com/zeropick/backend/category/Category.java
@@ -1,7 +1,23 @@
 package com.zeropick.backend.category;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "category")
+@Getter
+@NoArgsConstructor
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length=50)
+    private String name;
+
+    @Column(nullable = false, length=30)
+    private String code;
 }
--- src/main/java/com/zeropick/backend/category/CategoryRepository.java
+++ src/main/java/com/zeropick/backend/category/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.zeropick.backend.category;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByCodeIn(List<String> codes);
 }
