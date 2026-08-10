d9699c72f88ec083622f7d68bcde7c50d2efc927
mirikim404 <mirikim8989@gmail.com>
2026-08-10 10:54:45 +0900

    feat: SignupRequest, LoginRequest에 @NotBlank 추가

--- src/main/java/com/zeropick/backend/auth/dto/LoginRequest.java
+++ src/main/java/com/zeropick/backend/auth/dto/LoginRequest.java
@@ -1,9 +1,10 @@
 package com.zeropick.backend.auth.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-        @Email String email,
-        String password
+        @NotBlank @Email String email,
+        @NotBlank String password
 ) {
 }
