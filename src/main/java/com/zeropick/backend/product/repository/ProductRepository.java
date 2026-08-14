2a6cd3e577032585ce7e7080807db5f5fd7408ac
김아인 <clairekim0012@gmail.com>
2026-08-14 21:16:09 +0900

    feat: 상품 키워드 검색 API 구현 및 DTO 변환 로직 추가

--- src/main/java/com/zeropick/backend/product/repository/ProductRepository.java
+++ src/main/java/com/zeropick/backend/product/repository/ProductRepository.java
@@ -3,5 +3,10 @@ package com.zeropick.backend.product.repository;
 import com.zeropick.backend.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // 상품 이름(name)에 keyword가 포함된 항목들을 검색 (LIKE %keyword%)
+    List<Product> findByNameContaining(String keyword);
 }
