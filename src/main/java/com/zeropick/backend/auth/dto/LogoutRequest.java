27d77f77a57b21063b030d143e45f3f8eccc9081
mirikim404 <mirikim8989@gmail.com>
2026-08-17 14:44:45 +0900

    chore: 사용하지 않는 DTO 삭제

--- src/main/java/com/zeropick/backend/auth/dto/LogoutRequest.java
+++
@@ -1,5 +0,0 @@
-package com.zeropick.backend.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LogoutRequest(@NotBlank String refreshToken) { }
--- src/main/java/com/zeropick/backend/auth/dto/ReissueRequest.java
+++
@@ -1,6 +0,0 @@
-package com.zeropick.backend.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ReissueRequest(@NotBlank String refreshToken) {
-}
